5f32c169b58a30a6482d1133a05f4d0f3ee2ea71
Lucas Marques <lucasmarquesff@hotmail.com>
2021-09-15 16:58:37 -0300

    Creating specific requests with the @PathVariable annotation

--- src/main/java/com/eventapp/eventapp/controllers/EventController.java
+++ src/main/java/com/eventapp/eventapp/controllers/EventController.java
@@ -4,6 +4,7 @@ import com.eventapp.eventapp.models.Event;
 import com.eventapp.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,6 +33,14 @@ public class EventController {
         ModelAndView mv = new ModelAndView("index.html");
         Iterable<Event> events = er.findAll();
         mv.addObject("events", events);
+        return mv;
+    }
+
+    @RequestMapping("/{code}")
+    public ModelAndView detailsEvent(@PathVariable("code") long code) {
+        Event event = er.findByCode(code);
+        ModelAndView mv = new ModelAndView("event/detailsEvent.html");
+        mv.addObject("event", event);
         return mv;
     }
 }
