6ea80cc4a1e34aa227930a9a322b3e8e4b718613
Lucas Marques <lucasmarquesff@hotmail.com>
2021-09-15 14:50:24 -0300

    saving form data to the database - spring boot course (Michelli Brito)

--- src/main/java/com/eventapp/eventapp/EventappApplication.java
+++ src/main/java/com/eventapp/eventapp/EventappApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.eventapp.repository")
 @SpringBootApplication
 @EntityScan(basePackageClasses = {Event.class})
 public class EventappApplication {
--- src/main/java/com/eventapp/eventapp/controllers/EventController.java
+++ src/main/java/com/eventapp/eventapp/controllers/EventController.java
@@ -1,13 +1,28 @@
 package com.eventapp.eventapp.controllers;
 
+import com.eventapp.eventapp.models.Event;
+import com.eventapp.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EventController {
 
-    @RequestMapping("/registerEvent")
+    @Autowired
+    private EventRepository er;
+
+    @RequestMapping(value = "/registerEvent", method = RequestMethod.GET)
     public String form(){
         return "event/formEvent";
+    }
+
+    @RequestMapping(value = "/registerEvent", method = RequestMethod.POST)
+    public String form(Event event){
+
+        er.save(event);
+
+        return "redirect:/registerEvent";
     }
 }
