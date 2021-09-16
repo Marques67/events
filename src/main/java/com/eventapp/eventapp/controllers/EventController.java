f6d7084fde9f9aed9117e8a8c41e673cd98d85a6
Lucas Marques <lucasmarquesff@hotmail.com>
2021-09-16 15:11:47 -0300

    Saving data from an entity in the database related to another entity

--- src/main/java/com/eventapp/eventapp/controllers/EventController.java
+++ src/main/java/com/eventapp/eventapp/controllers/EventController.java
@@ -1,7 +1,9 @@
 package com.eventapp.eventapp.controllers;
 
 import com.eventapp.eventapp.models.Event;
+import com.eventapp.eventapp.models.Guest;
 import com.eventapp.repository.EventRepository;
+import com.eventapp.repository.GuestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,9 @@ public class EventController {
 
     @Autowired
     private EventRepository er;
+
+    @Autowired
+    private GuestRepository gr;
 
     @RequestMapping(value = "/registerEvent", method = RequestMethod.GET)
     public String form(){
@@ -36,11 +41,19 @@ public class EventController {
         return mv;
     }
 
-    @RequestMapping("/{code}")
+    @RequestMapping(value = "/{code}", method = RequestMethod.GET)
     public ModelAndView detailsEvent(@PathVariable("code") long code) {
         Event event = er.findByCode(code);
         ModelAndView mv = new ModelAndView("event/detailsEvent.html");
         mv.addObject("event", event);
         return mv;
+    }
+
+    @RequestMapping(value = "/{code}", method = RequestMethod.POST)
+    public String detailsEventPost(@PathVariable("code") long code, Guest guest) {
+        Event event = er.findByCode(code);
+        guest.setEvent(event);
+        gr.save(guest);
+        return "redirect:/{code}";
     }
 }
---
+++ src/main/java/com/eventapp/repository/GuestRepository.java
@@ -0,0 +1,10 @@
+package com.eventapp.repository;
+
+import com.eventapp.eventapp.models.Guest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, String> {
+
+}
